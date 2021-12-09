6ec39b6758fd93a6c45faa0e44c6b4fbe03451d4
ohtaman <ohtamans@gmail.com>
2021-12-09 09:29:58 +0900

    Update

--- st_request_header/app.py
+++ st_request_header/app.py
@@ -6,7 +6,7 @@ from streamlit.server.server import Server
 def main():
     session_infos = Server.get_current()._session_info_by_id.values()
     headers = [info.ws.request.headers for info in session_infos]
-    st.write(pd.DataFrame(headers))
+    st.write(pd.DataFrame(headers).transpose())
 
 
 if __name__ == '__main__':
