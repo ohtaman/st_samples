51395966c1f74a7fcf36712f7af33f5035f33d5a
ohtaman <ohtamans@gmail.com>
2021-12-14 15:37:01 +0900

    Update app.py

--- st_upload/app.py
+++ st_upload/app.py
@@ -1,27 +1,25 @@
+import os
+
 import streamlit as st
-import pandas as pd
+from PIL import Image
+
+
+IMG_PATH = 'imgs'
 
 
 def main():
-    st.markdown('# File Upload example')
-    file = st.file_uploader('Upload csv/tsv/txt file.', type=['csv', 'tsv', 'txt'])
+    st.markdown('# 画像を保存するデモ')
+    file = st.file_uploader('画像をアップロードしてください.', type=['jpg', 'jpeg', 'png'])
     if file:
-        st.write(f'file type: {file.type}')
-        if file.type == 'text/csv':
-            delimiter = ','
-        elif file.type in ('text/tab-separated-values', 'text/plain'):
-            delimiter = '\t'
-        else:
-            delimiter = ','
-
-        data = pd.read_csv(
-            file,
-            delimiter=delimiter
-        )
-        columns = st.multiselect(data.columns)
-        filtered_data = data[columns]
-        st.write(filtered_data)
-        st.download_button('ダウンロード', filtered_data)
+        st.markdown(f'{file.name} をアップロードしました.')
+        img_path = os.path.join(IMG_PATH, file.name)
+        # 画像を保存する
+        with open(img_path, 'wb') as f:
+            f.write(file.read())
+            
+        # 保存した画像を表示
+        img = Image.open(img_path)
+        st.image(img)
 
 if __name__ == '__main__':
     main()
