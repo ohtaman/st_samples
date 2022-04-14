05b39ca001440c4e1516a958dfdb9c2ab799c2c3
ohtaman <ohtamans@gmail.com>
2022-04-14 15:16:33 +0900

    Update st_sortables.

--- st_sortables/app.py
+++ st_sortables/app.py
@@ -3,36 +3,40 @@ from streamlit_sortables import sort_items
 
 import streamlit as st
 
-st.title('Sortables')
-
-st.write('Sort items in a single container.')
-items = ['item1', 'item2', 'item3']
-sorted_items = sort_items(items)
-st.write(sorted_items)
+st.title('streamlit-sortables')
 
 
-st.write('----')
-st.write('Sort items in multiple containers.')
-items = [
-    {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
-    {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
-]
-sorted_items = sort_items(items, multi_containers=True)
-st.write(sorted_items)
+st.markdown('Sort items in a single container.')
+with st.echo():
+    items = ['item1', 'item2', 'item3']
+    sorted_items = sort_items(items)
+    st.write(sorted_items)
 
-st.write('----')
-st.write('Lots of items in a single container.')
-items = [
-    {'header': 'header1', 'items': ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13']},
-]
-sorted_items = sort_items(items, multi_containers=True)
-st.write(sorted_items)
+st.markdown('----')
+st.markdown('Sort items in multiple containers.')
+with st.echo():
+    items = [
+        {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
+        {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
+    ]
+    sorted_items = sort_items(items, multi_containers=True)
+    st.write(sorted_items)
 
-st.write('----')
-st.write('Sort items in multiple containers with vertical direction.')
-items = [
-    {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
-    {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
-]
-sorted_items = sort_items(items, multi_containers=True, direction="vertical")
-st.write(sorted_items)
+st.markdown('----')
+st.markdown('Lots of items in a single container.')
+with st.echo():
+    items = [
+        {'header': 'header1', 'items': ['item1', 'item2', 'item3', 'item4', 'item5', 'item6', 'item7', 'item8', 'item9', 'item10', 'item11', 'item12', 'item13']},
+    ]
+    sorted_items = sort_items(items, multi_containers=True)
+    st.write(sorted_items)
+
+st.markdown('----')
+st.markdown('Sort items in multiple containers with vertical direction.')
+with st.echo():
+    items = [
+        {'header': 'container1', 'items': ['item1', 'item2', 'item3']},
+        {'header': 'container2', 'items': ['item4', 'item5', 'item6']},
+    ]
+    sorted_items = sort_items(items, multi_containers=True, direction='vertical')
+    st.write(sorted_items)
